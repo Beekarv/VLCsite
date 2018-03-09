0f5903db4cda257ee62c4ccd3859559935d37cb3
Renan Mayrinck <renan.mtk@gmail.com>
2018-03-09 01:26:11 -0300

    Alterações diversas

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -2,13 +2,7 @@
 // DEFINA VARIAVEIS ABAIXO
 //////////////////////////
 
-wow = new WOW({
-              boxClass:     'wow',      // default
-              animateClass: 'animated', // default
-              offset:       1000,       // default is 0
-              mobile:       true,       // default
-              live:         true        // default
-              })
+
 
 //////////////////////////
 // SCRIPTS
